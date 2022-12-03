c751026d374c3ed9358ecb9405a5e0d92dc54bbe
SuperAtraction <91375731+SuperAtraction@users.noreply.github.com>
2022-12-03 14:35:54 +0100

    Css by Body

--- Code&Or/files/app.js
+++ Code&Or/files/app.js
@@ -3,6 +3,4 @@ function apploader(){
     document.querySelector("#chargement").style.display = "none";
     document.querySelector("body").style.visibility = "visible";
     document.title="Olop Code&Or - Loading...";
-    document.querySelector("body").style.backgroundColor = "rgb(70,70,70);";
-    document.querySelector("body").style.color = "rgb(255,255,255);";
 }
