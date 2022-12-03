790f06edd80acde7d21d9f826037bac2e7e20920
SuperAtraction <91375731+SuperAtraction@users.noreply.github.com>
2022-12-03 13:48:41 +0100

    No #Load

--- Code&Or/files/app.js
+++ Code&Or/files/app.js
@@ -1,7 +1,6 @@
 apploader();
 function apploader(){
     document.querySelector("#chargement").style.display = "none";
-                document.querySelector("#load").style.display = "none";
                 document.querySelector("body").style.visibility = "visible";
                 document.body.background.color="rgb(70, 70, 70);";
           document.body.color="rgb(255, 255, 255)";
