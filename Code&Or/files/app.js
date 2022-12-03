64d3af90df2f32cd374f62bcf49f8e7d713d968a
SuperAtraction <91375731+SuperAtraction@users.noreply.github.com>
2022-12-03 14:05:05 +0100

    CSS Update

--- Code&Or/files/app.js
+++ Code&Or/files/app.js
@@ -1,6 +1,8 @@
 apploader();
 function apploader(){
     document.querySelector("#chargement").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+    document.title="Olop Code&Or - Loading...";
                 document.querySelector("body").style.visibility = "visible";
           document.title="Olop Code&Or - Loading...";
 }
