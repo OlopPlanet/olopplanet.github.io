07ace627d743e8308eb0424e2ac7db0748c26f79
SuperAtraction <91375731+SuperAtraction@users.noreply.github.com>
2022-12-03 13:43:13 +0100

    AppLoader

--- Code&Or/files/app.js
+++ Code&Or/files/app.js
@@ -1,3 +1,10 @@
+apploader();
 function apploader(){
+    ocument.querySelector("#chargement").style.display = "none";
+                document.querySelector("#load").style.display = "none";
+                document.querySelector("body").style.visibility = "visible";
+                document.body.background.color="rgb(70, 70, 70);";
+          document.body.color="rgb(255, 255, 255)";
+          document.title="Olop Code&Or - Loading...";
     alert("Loaded !!");
 }
