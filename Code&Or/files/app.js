56752b9dda441e76f6f55f4d1967e9f7a3e47c38
SuperAtraction <91375731+SuperAtraction@users.noreply.github.com>
2022-12-03 14:02:41 +0100

    Backoclor

--- Code&Or/files/app.js
+++ Code&Or/files/app.js
@@ -2,7 +2,5 @@ apploader();
 function apploader(){
     document.querySelector("#chargement").style.display = "none";
                 document.querySelector("body").style.visibility = "visible";
-                document.body.background.color="rgb(70, 70, 70);";
-          document.body.color="rgb(255, 255, 255)";
           document.title="Olop Code&Or - Loading...";
 }
